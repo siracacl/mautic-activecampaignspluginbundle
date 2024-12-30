8e7b2da48695d71e37ae75bc846a3a329d9b14d9
siracacl <129221624+siracacl@users.noreply.github.com>
2024-12-30 10:12:46 +0100

    Create ApiController.php

---
+++ ActiveCampaignsPluginBundle/Controller/ApiController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MauticPlugin\ActiveCampaignsPluginBundle\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Mautic\ApiBundle\Controller\CommonApiController;
+
+class ApiController extends CommonApiController
+{
+    public function getActiveCampaignsAction($id)
+    {
+        // Database connection
+        $connection = $this->getDoctrine()->getConnection();
+
+        // sql query
+        $sql = '
+            SELECT c.id AS campaign_id, c.name, cl.lead_id AS lead_id, cl.date_added, c.description, cl.manually_removed
+            FROM campaign_leads cl
+            INNER JOIN campaigns c ON cl.campaign_id = c.id
+            WHERE cl.lead_id = :contactId
+              AND c.is_published = 1
+              AND cl.date_last_exited IS NULL
+              AND cl.manually_removed = 0
+        ';
+
+        // query
+        $result = $connection->executeQuery($sql, ['contactId' => $id]);
+        $campaigns = $result->fetchAllAssociative(); // fetch results
+
+        // return in JSON
+        return new JsonResponse($campaigns);
+    }
+}
